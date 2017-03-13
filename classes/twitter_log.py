75126fd56fcc03eca3c7d4c1ea7fff18eff892fc
nachocarracedo <nachocarracedo@yahoo.es>
2017-03-12 20:21:40 -0400

    classes2

--- classes/twitter_log.py
+++ classes/twitter_log.py
@@ -1,7 +1,6 @@
 class Twitter_log_csv:
-							
+			
 	def save_csv(self, last_tweets_df,tweet_hits_df):
-		
 		# save last tweet				
 		last_tweets_df.to_csv("./csv/last_tweet.csv", index=False, encoding='utf-8')
 		# save alerts sent
